b4347010729891813617fd022700f274b07bffc4
ajy1216 <ajy1216@snu.ac.kr>
2025-10-23 17:36:39 +0900

    fix assignment model field & makemigration

--- backend/assignments/models.py
+++ backend/assignments/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 
 class Assignment(models.Model):
-    # TODO: 반드시!!!!!! 반드시!!!!!!! null과 blank가 True인 옵션을 지워주셔야합니다!!!!!!
-    # TODO: s3 연결 & 과제 생성 api test를 위해서 임시로 course_class가 없는 상태에서 "테스트 목적"으로 허용한 설정이기 때문에 반드시 제거해야합니다!!!!!!!!
     course_class = models.ForeignKey("courses.CourseClass", on_delete=models.CASCADE, related_name="assignments")
     topics = models.ManyToManyField("catalog.Topic", blank=True, related_name="assignments")
     title = models.CharField(max_length=255)
@@ -12,6 +10,7 @@ class Assignment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     visible_from = models.DateTimeField()
     due_at = models.DateTimeField()
+    grade = models.CharField(max_length=16, blank=True)
 
     class Meta:
         indexes = [
